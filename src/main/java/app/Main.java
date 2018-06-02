ce2c0ef7fd361de78947a943a07670ab443bab33
davidovichmarcos <davidovich.marcos@gmail.com>
2018-06-01 21:26:41 -0300

    Adding more functions to fileHelper

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -1,25 +1,15 @@
 package app;
 
+import app.helpers.FileHelper;
+
 import java.time.LocalDate;
+import java.util.TreeSet;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        Hotel h = new Hotel();
-        Passenger p1 = new Passenger("Juan", "Adolfo", 35111333, "pepe@javie.com", "Micalle 2344, Mar del Plata.");
-        Booking book1= new Booking(LocalDate.of(2018, 06, 23), 4, p1, 1, true);
-        System.out.println(book1);
-
-
-        h.rooms.add(new Room(1,4));
-        h.checkIn( p1 , 1, 4);
-        System.out.println(h.rooms.get(0));
-        h.rooms.get(0).addIntake(120);
-        System.out.println("Habitacion cerrada. Su Cuenta es $ "+ h.checkOut(1));
-        System.out.println(h.rooms.get(0));
-
-        //h.bookings.add(book1);
+       
 
     }
 }
--- src/main/java/app/helpers/FileHelper.java
+++ src/main/java/app/helpers/FileHelper.java
@@ -1,23 +1,22 @@
 package app.helpers;
 
 import app.Employee;
+import app.Passenger;
 import app.Room;
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import com.google.gson.JsonObject;
 
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.TreeSet;
 
 public class FileHelper {
-    File employeesFile = new File("C:\\Users\\Marcos\\IdeaProjects\\HotelCalifornia\\src\\main\\java\\app\\files\\EmployeesFile");
-    File adminsFile = new File("C:\\Users\\Marcos\\IdeaProjects\\HotelCalifornia\\src\\main\\java\\app\\files\\AdminsFile");
-    File bookingsFile = new File("C:\\Users\\Marcos\\IdeaProjects\\HotelCalifornia\\src\\main\\java\\app\\files\\BookingsFile");
-    File passengersFile = new File("C:\\Users\\Marcos\\IdeaProjects\\HotelCalifornia\\src\\main\\java\\app\\files\\PassengersFile");
-    File roomsFile = new File("C:\\Users\\Marcos\\IdeaProjects\\HotelCalifornia\\src\\main\\java\\app\\files\\RoomsFile");
+    File employeesFile = new File(".\\src\\main\\java\\app\\files\\EmployeesFile");
+    File adminsFile = new File(".\\src\\main\\java\\app\\files\\AdminsFile");
+    File bookingsFile = new File(".\\src\\main\\java\\app\\files\\BookingsFile");
+    File passengersFile = new File(".\\src\\main\\java\\app\\files\\PassengersFile");
+    File roomsFile = new File(".\\src\\main\\java\\app\\files\\RoomsFile");
 
     /**
      * Saves the treeset of employees in the employeesFile.
@@ -27,10 +26,46 @@ public class FileHelper {
     public void employeesToJsonFile(TreeSet<Employee> employees) {
         try {
             BufferedWriter writer = new BufferedWriter(new FileWriter(employeesFile));
+            GsonBuilder gsonBuilder = new GsonBuilder();
+            gsonBuilder.setPrettyPrinting();
+            Gson gson = gsonBuilder.create();
+            gson.toJson(employees, TreeSet.class, writer);
+            writer.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Get the employees from the Json file.
+     *
+     * @return TreeSet of employees.
+     */
+    public TreeSet<Employee> getEmployeesfromJson() {
+        TreeSet<Employee> employees = null;
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(employeesFile));
             Gson gson = new Gson();
-            for (Employee employee : employees) {
-                gson.toJson(employee, Employee.class, writer);
-            }
+            employees = gson.fromJson(reader, TreeSet.class);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        return employees;
+    }
+
+    /**
+     * Saves the Arraylist of rooms in the roomsFile.
+     *
+     * @param rooms The arrayList of rooms.
+     */
+    public void roomsToJsonFile(ArrayList<Room> rooms) {
+        try {
+            BufferedWriter writer = new BufferedWriter(new FileWriter(roomsFile));
+            GsonBuilder gsonBuilder = new GsonBuilder();
+            gsonBuilder.setPrettyPrinting();
+            Gson gson = gsonBuilder.create();
+            gson.toJson(rooms, ArrayList.class, writer);
             writer.close();
         } catch (IOException e) {
             e.printStackTrace();
@@ -38,20 +73,56 @@ public class FileHelper {
     }
 
     /**
-     * Saves the Arraylist of rooms in the roomsFile.
+     * Get the rooms from the Json file.
      *
-     * @param rooms The treeset of rooms.
+     * @return The ArrayList of rooms.
      */
-    public void roomsToJsonFile(ArrayList<Room> rooms) {
+    public ArrayList<Room> getRoomsFromJson() {
+        ArrayList<Room> rooms = null;
         try {
-            BufferedWriter writer = new BufferedWriter(new FileWriter(roomsFile));
+            BufferedReader reader = new BufferedReader(new FileReader(roomsFile));
             Gson gson = new Gson();
-            for (Room room : rooms) {
-                gson.toJson(room, Room.class, writer);
-            }
+            rooms = gson.fromJson(reader, ArrayList.class);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        return rooms;
+    }
+
+    /**
+     * Saves the treeset of passengers in the passengersFile.
+     *
+     * @param passengers The treeset of passengers.
+     */
+    public void passengersToJsonFile(TreeSet<Passenger> passengers) {
+        try {
+            BufferedWriter writer = new BufferedWriter(new FileWriter(passengersFile));
+            GsonBuilder gsonBuilder = new GsonBuilder();
+            gsonBuilder.setPrettyPrinting();
+            Gson gson = gsonBuilder.create();
+            gson.toJson(passengers, TreeSet.class, writer);
             writer.close();
+
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
+
+    /**
+     * Get the passengers from the Json file.
+     *
+     * @return TreeSet of employees.
+     */
+    public TreeSet<Passenger> getPassengersfromJson() {
+        TreeSet<Passenger> passengers = null;
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(passengersFile));
+            Gson gson = new Gson();
+            passengers = gson.fromJson(reader, TreeSet.class);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        return passengers;
+    }
+
 }
