5288a3cd99513babd33999f94fe0a79b68a1710a
davidovichmarcos <davidovich.marcos@gmail.com>
2018-06-23 21:00:23 -0300

    adding main

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -18,9 +18,9 @@ import javafx.stage.StageStyle;
 
 
 public class Main extends Application {
-	
+
     private Stage window;
-   
+
     Parent root;
     AnchorPane pane;
 
@@ -29,21 +29,21 @@ public class Main extends Application {
     public static void main(String[] args) {
         Application.launch(args);
     }
-    
-	@Override
+
+    @Override
     public void start(Stage primaryStage) throws Exception {
-		window = primaryStage;
-		
-		FXMLLoader loader = new FXMLLoader();
+        window = primaryStage;
+
+        FXMLLoader loader = new FXMLLoader();
         loader.setLocation(Main.class.getResource("/fxml/main.fxml"));
         AnchorPane login = (AnchorPane) loader.load();
         window.setTitle("Welcome");
         window.setScene(new Scene(login));
         window.sizeToScene();
-        window.setResizable(false); 
-        
+        window.setResizable(false);
+
         window.show();
-	}
-	
-	
+    }
+
+
 }
